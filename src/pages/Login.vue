008d720f68b355b7493cd41db85afb99063e259f
bluedistro <bineykingsley36@gmail.com>
2020-02-02 18:03:41 +0000

    enhanced login a bit

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,33 +2,64 @@
 <div class="row q-pa-md flex flex-center">
   <div class=" col-md-6 col-sm-12 col-12 col-lg-6 login-div">
 
-    <q-form
-      @submit="onSubmit"
-      class="q-gutter-md"
-    >
-      <q-input
-        standard
-        v-model="username"
-        label="username *"
-        hint="enter your username"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type username']"
-      />
+    <q-card class="form-card">
+        <q-card-section>
+        <q-item>
+            <q-item-section avatar>
+            <q-avatar>
+                <img src="../assets/clogo.jpeg">
+            </q-avatar>
+            </q-item-section>
 
-      <q-input
-        standard
-        type="password"
-        v-model="password"
-        hint="enter your password"
-        label="enter password *"
-        lazy-rules
-        :rules="[ val => val !== null && val !== '' || 'Please type password' ]"
-      />
+            <q-item-section>
+            <q-item-label>Membership Registration Portal Login</q-item-label>
+            <q-item-label caption>
+                Presbyterian Church of Ghana, Millenium City Congregation, Kasoa.
+            </q-item-label>
+            </q-item-section>
+        </q-item>
+        </q-card-section>
+        <q-separator />
+        <q-card-section>
+            <q-form
+            @submit="onSubmit"
+            class="q-gutter-md"
+            >
+            <q-input
+                standard
+                dense
+                v-model="username"
+                label="username *"
+                hint="enter your username"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type username']"
+            />
 
-      <div>
-        <q-btn label="login" type="submit" color="primary"/>
-      </div>
-    </q-form>
+            <q-input
+                standard
+                dense
+                type="password"
+                v-model="password"
+                hint="enter your password"
+                label="enter password *"
+                lazy-rules
+                :rules="[ val => val !== null && val !== '' || 'Please type password' ]"
+            />
+
+            <div>
+                <q-btn  label="login"
+                        type="submit"
+                        :loading="loginLoading"
+                        @click="onSubmit"
+                        color="primary">
+                        <template v-slot:loading>
+                            <q-spinner-hourglass class="on-left" />
+                        </template>
+                </q-btn>
+            </div>
+            </q-form>
+        </q-card-section>
+    </q-card>
 
   </div>
 </div>
@@ -40,23 +71,33 @@ export default {
     return {
       username: null,
       password: null,
-
+      loginLoading: false,
       accept: false
     }
   },
 
   methods: {
     onSubmit () {
+      this.loginLoading = true
       let user = { username: this.username, password: this.password }
       this.$store.dispatch('example/login', user)
         .then(resp => {
           let loginStatus = this.$store.getters['example/getLoginSuccess']
           if (loginStatus === true) {
             this.$router.push({ name: 'home' })
+            this.loginLoading = false
           }
         })
         .catch(error => {
+          this.loginLoading = false
           console.log('error ', error)
+          this.$q.notify({
+            color: 'red-4',
+            position: 'top',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'Unable to login. Please check login credentials and try again'
+          })
         })
     }
   }
@@ -65,4 +106,10 @@ export default {
 <style lang="stylus" scoped>
 .login-div
     margin-top 60px
+
+.form-card
+    border solid $grey-4 1px
+    border-radius 4px
+    background-color $grey-1
+
 </style>
