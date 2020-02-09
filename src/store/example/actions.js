bb3da8b7b6f662ac4e43d06130544c095461cf85
bluedistro <bineykingsley36@gmail.com>
2020-02-09 07:14:59 +0000

    temp config

--- src/store/example/actions.js
+++ src/store/example/actions.js
@@ -2,20 +2,20 @@ import axios from 'axios'
 
 export function login ({ commit }, user) {
   return new Promise((resolve, reject) => {
-    const params = { username: user.username, password: user.password }
-    let path = 'http://localhost:5000/api/login'
-    axios.post(path, params).then(resp => {
-      console.log('resp ', resp.data.success)
-      if (/* resp.data.success === true */ user.username === 'kasoapresby' && user.password === 'kasoa33124') {
-        commit('loginSuccess', true)
-      } else {
-        commit('loginSuccess', false)
-      }
-      resolve(resp)
-    }).catch(error => {
+    // const params = { username: user.username, password: user.password }
+    // let path = 'http://localhost:5000/api/login'
+    // axios.post(path, params).then(resp => {
+    // console.log('resp ', resp.data.success)
+    if (/* resp.data.success === true */ user.username === 'kasoapresby' && user.password === 'kasoa33124') {
+      commit('loginSuccess', true)
+    } else {
       commit('loginSuccess', false)
-      reject(error)
-    })
+    }
+    resolve('okay')
+    // }).catch(error => {
+    //   commit('loginSuccess', false)
+    //   reject(error)
+    // })
   })
 }
 
